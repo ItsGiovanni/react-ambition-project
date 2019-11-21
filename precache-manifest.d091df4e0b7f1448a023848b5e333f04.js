8f4c5913bc3f9d57e493413e6a588fd3fb6a2987
ItsGiovanni <giovannidogger@gmail.com>
2019-11-21 14:09:18 +0100

    Updates

---
+++ precache-manifest.d091df4e0b7f1448a023848b5e333f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e69ee883376511da2de5828eb0d42e",
+    "url": "/react-ambition-project/index.html"
+  },
+  {
+    "revision": "8239b64792d113b47113",
+    "url": "/react-ambition-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e3e58e6c604f02020380",
+    "url": "/react-ambition-project/static/js/2.d419045c.chunk.js"
+  },
+  {
+    "revision": "8239b64792d113b47113",
+    "url": "/react-ambition-project/static/js/main.4054970f.chunk.js"
+  },
+  {
+    "revision": "0b5b6bf583e7d4684418",
+    "url": "/react-ambition-project/static/js/runtime-main.ad2fdbe4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-ambition-project/static/media/logo.25bf045c.svg"
+  }
+]);
